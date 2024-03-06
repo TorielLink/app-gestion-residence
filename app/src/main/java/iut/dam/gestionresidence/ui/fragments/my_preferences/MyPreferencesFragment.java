196680ea5a16438b71720201d844fe3bca25ec9b
TorielLink <115531902+TorielLink@users.noreply.github.com>
2024-03-06 11:37:54 +0100

    Avancement du fragment MesPréférences

--- app/src/main/java/iut/dam/gestionresidence/ui/fragments/my_preferences/MyPreferencesFragment.java
+++ app/src/main/java/iut/dam/gestionresidence/ui/fragments/my_preferences/MyPreferencesFragment.java
@@ -1,37 +1,15 @@
 package iut.dam.gestionresidence.ui.fragments.my_preferences;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
 
-import iut.dam.gestionresidence.databinding.FragmentMyPreferencesBinding;
+import iut.dam.gestionresidence.R;
 
-public class MyPreferencesFragment extends Fragment {
-
-    private FragmentMyPreferencesBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        MyPreferencesViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(MyPreferencesViewModel.class);
-
-        binding = FragmentMyPreferencesBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textMyPreferences;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
-
+public class MyPreferencesFragment extends PreferenceFragmentCompat {
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 }
