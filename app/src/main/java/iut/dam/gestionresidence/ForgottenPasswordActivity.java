3b0516bdd3775c44dbd5c4fd92d0f66d446fecc8
Rémi <contact@remilemaire.fr>
2024-03-06 12:33:51 +0100

    password forgotten

--- app/src/main/java/iut/dam/gestionresidence/ForgottenPasswordActivity.java
+++ app/src/main/java/iut/dam/gestionresidence/ForgottenPasswordActivity.java
@@ -1,13 +1,65 @@
 package iut.dam.gestionresidence;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ForgottenPasswordActivity  extends AppCompatActivity {
+    private static final String URL = "http://remi-lem.alwaysdata.net/amenagor/getPassword.php";//TODO
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Mot de passe oublié");
         setContentView(R.layout.activity_forgotten_password);
+
+        EditText etEmail = findViewById(R.id.editTextEmailPasswordForgotten);
+        Button btnLogin = findViewById(R.id.btnPasswordForgotten);
+
+        btnLogin.setOnClickListener(view -> {
+            String email = etEmail.getText().toString().trim();
+
+            String urlString = "http://remi-lem.alwaysdata.net/amenagor/getPassword.php?email="+email;
+            Ion.with(this).load(urlString).asString().setCallback((e, result) -> {
+                if(result == null)
+                    Log.d(TAG, "No response from the server!!!");
+                else {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(result);
+                        String password = jsonObject.getString("password");
+                        new AlertDialog.Builder(this)
+                                .setTitle("Password")//TODO LOCALIZE
+                                .setMessage("Your password is: " + password)
+                                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                                    dialog.dismiss();
+                                    finish();
+                                })
+                                .setOnDismissListener(dialog -> finish())
+                                .show();
+                    } catch (JSONException ex) {
+                        new AlertDialog.Builder(this)
+                                .setTitle("Password")//TODO LOCALIZE
+                                .setMessage("Your account does not exist")
+                                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                                    dialog.dismiss();
+                                    finish();
+                                })
+                                .setOnDismissListener(dialog -> finish())
+                                .show();
+                    }
+                }
+            });
+        });
     }
 }
