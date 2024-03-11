504e00a72585dd642949264168342f3a77527f71
TorielLink <115531902+TorielLink@users.noreply.github.com>
2024-03-11 15:23:43 +0100

    Compte utilisateur quasi-fini
    ______
    PROBLÈME :
    - Traiter les chaînes vides

--- app/src/main/java/iut/dam/gestionresidence/ui/fragments/user_account/UserAccountFragment.java
+++ app/src/main/java/iut/dam/gestionresidence/ui/fragments/user_account/UserAccountFragment.java
@@ -1,30 +1,29 @@
 package iut.dam.gestionresidence.ui.fragments.user_account;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.koushikdutta.ion.Ion;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 
 import iut.dam.gestionresidence.R;
-import iut.dam.gestionresidence.adapter.HabitatAdapter;
 import iut.dam.gestionresidence.databinding.FragmentUserAccountBinding;
-import iut.dam.gestionresidence.entities.Appliance;
-import iut.dam.gestionresidence.entities.Habitat;
 import iut.dam.gestionresidence.entities.TokenManager;
-import iut.dam.gestionresidence.entities.User;
 
 public class UserAccountFragment extends Fragment {
     private FragmentUserAccountBinding binding;
@@ -43,7 +42,7 @@ public class UserAccountFragment extends Fragment {
                 + TokenManager.getToken();
 
         Ion.with(this).load(urlString).asString().setCallback((e, result) -> {
-            JSONObject jsonObject = null;
+            JSONObject jsonObject;
             Log.d("debugRemi", urlString);
             try {
                 jsonObject = new JSONObject(result);
@@ -58,17 +57,67 @@ public class UserAccountFragment extends Fragment {
                 new AlertDialog.Builder(getActivity())
                         .setTitle(getString(R.string.menu_user_account))
                         .setMessage(getString(R.string.error_get_info_account))
-                        .setPositiveButton(android.R.string.ok, (dialog, which) -> {
-                            dialog.dismiss();
-                        })
+                        .setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss())
                         .show();
             }
         });
 
-        binding.buttonUpdatePassword.setOnClickListener(v -> {
-            // TODO: Ajouter màj mdp
+        CheckBox checkBoxFirstName = binding.checkboxFirstName;
+        CheckBox checkBoxLastName = binding.checkboxLastName;
+        CheckBox checkboxPassword = binding.checkboxPassword;
+
+        EditText editTextFirstName = binding.editTextFirstName;
+        EditText editTextLastName = binding.editTextLastName;
+        EditText editTextPassword = binding.editTextPassword;
+
+        binding.btnConfirmChanges.setOnClickListener(v -> {
+            //TODO: récupérer les anciennes valeurs (ou gérer depuis serveur les chaînes vides)
+            String firstName = String.valueOf(editTextFirstName.getText());
+            String lastName = String.valueOf(editTextLastName.getText());
+            String password = String.valueOf(editTextPassword.getText());
+
+            if (checkBoxFirstName.isChecked())
+                firstName = String.valueOf(binding.editTextFirstName.getText());
+            if (checkBoxLastName.isChecked())
+                lastName = String.valueOf(binding.editTextLastName.getText());
+            if (checkboxPassword.isChecked())
+                password = String.valueOf(binding.editTextPassword.getText());
+
+            String urlStringUserModif =
+                    "http://remi-lem.alwaysdata.net/gestionResidence/changeUserParameters.php?token="
+                    + TokenManager.getToken() + "&firstname=" + firstName + "&lastname=" + lastName
+                    + "&password=" + password;
+
+            Ion.with(this).load(urlStringUserModif).asString().setCallback((e, result) -> {
+                if(result == null)
+                    Log.d(TAG, "No response from the server!!!");
+                else {
+                    if(result.equals("OK")) {
+                        new AlertDialog.Builder(getActivity())
+                                .setTitle(getString(R.string.success_change_user_parameters))
+                                .setPositiveButton(android.R.string.ok, (dialog, which) ->
+                                        dialog.dismiss())
+                                .show();
+                    }
+                    else {
+                        new AlertDialog.Builder(getActivity())
+                                .setTitle(getString(R.string.error_change_user_parameters))
+                                .setPositiveButton(android.R.string.ok,
+                                        (dialog, which) -> dialog.dismiss())
+                                .show();
+                    }
+                }
+            });
         });
+
+        checkBoxFirstName.setOnCheckedChangeListener((buttonView, isChecked) ->
+                editTextFirstName.setEnabled(isChecked));
+        checkBoxLastName.setOnCheckedChangeListener((buttonView, isChecked) ->
+                editTextLastName.setEnabled(isChecked));
+        checkboxPassword.setOnCheckedChangeListener((buttonView, isChecked) ->
+                editTextPassword.setEnabled(isChecked));
     }
+
 
     @Override
     public void onDestroyView() {
