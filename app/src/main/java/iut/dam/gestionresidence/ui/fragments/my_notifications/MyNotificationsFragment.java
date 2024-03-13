2214310e9f19d2d9373f171c9870f16f8cc1e4f3
Rémi <contact@remilemaire.fr>
2024-03-13 12:40:57 +0100

    export des strings de notifs

--- app/src/main/java/iut/dam/gestionresidence/ui/fragments/my_notifications/MyNotificationsFragment.java
+++ app/src/main/java/iut/dam/gestionresidence/ui/fragments/my_notifications/MyNotificationsFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
 import java.util.List;
 
+import iut.dam.gestionresidence.R;
 import iut.dam.gestionresidence.adapter.NotificationAdapter;
 import iut.dam.gestionresidence.databinding.FragmentListHabitatsBinding;
 import iut.dam.gestionresidence.databinding.FragmentMyNotificationsBinding;
@@ -30,9 +31,8 @@ public class MyNotificationsFragment extends Fragment {
         ListView listView = binding.listNotifications;
 
         List<Notification> itemList = new ArrayList<>();
-        //TODO
-        itemList.add(new Notification("Alerte de dépassement", "Dépassement de wattage pour le créneau sélectionné"));
-        itemList.add(new Notification("Libération d'un créneau", "Un voisin a libéré un des créneaux que vous désiriez"));
+        itemList.add(new Notification(getString(R.string.notif_overrun), getString(R.string.notif_overrun_body)));
+        itemList.add(new Notification(getString(R.string.notif_slot), getString(R.string.notif_slot_body)));
 
         NotificationAdapter adapter = new NotificationAdapter(requireContext(), itemList);
 
