11ec7f6deb76f1520e7db47a1a14609ca6a4458e
TorielLink <115531902+TorielLink@users.noreply.github.com>
2024-03-25 23:21:13 +0100

    Activation / Désactivation des notifications (dans les préférences) avec une petite notif pour dire que ça a été fait

--- app/src/main/java/iut/dam/gestionresidence/MainActivity.java
+++ app/src/main/java/iut/dam/gestionresidence/MainActivity.java
@@ -2,11 +2,15 @@ package iut.dam.gestionresidence;
 
 import static android.content.ContentValues.TAG;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,7 +18,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -145,19 +152,33 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         assert key != null;
-        if (key.equals("pref_key_language")) {
-            String language = sharedPreferences.getString(key, "fr");
-            setLocale(language);
-            recreate(); // Reload the activity to apply the language change
-        } else if (key.equals("pref_key_theme")) {
-            String theme = sharedPreferences
-                    .getString("pref_key_theme", "light");
-            if (theme.equals("dark")) {
-                setTheme(R.style.Theme_GestionResidence_Dark);
-            } else {
-                setTheme(R.style.Theme_GestionResidence);
-            }
-            recreate();
+        switch (key) {
+            case "pref_key_language":
+                String language = sharedPreferences.getString(key, "fr");
+                setLocale(language);
+                recreate(); // Reload the activity to apply the language change
+
+                break;
+            case "pref_key_theme":
+                String theme = sharedPreferences
+                        .getString("pref_key_theme", "light");
+                if (theme.equals("dark")) {
+                    setTheme(R.style.Theme_GestionResidence_Dark);
+                } else {
+                    setTheme(R.style.Theme_GestionResidence);
+                }
+                recreate();
+                break;
+            case "pref_key_notification":
+                boolean notificationsEnabled = sharedPreferences.getBoolean(key, false);
+                if (notificationsEnabled) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        enableNotifications();
+                    }
+                } else {
+                    disableNotifications();
+                }
+                break;
         }
     }
 
@@ -167,6 +188,45 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         Configuration configuration = new Configuration();
         configuration.setLocale(locale);
         getResources().updateConfiguration(configuration, getResources().getDisplayMetrics());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void enableNotifications() {
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+        if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+            startActivity(intent);
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.notif_channel_name);
+            String description = getString(R.string.notif_channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("YOUR_CHANNEL_ID", name,
+                    importance);
+            channel.setDescription(description);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
+                "YOUR_CHANNEL_ID")
+                .setSmallIcon(R.drawable.ic_menu_third)
+                .setContentTitle(getString(R.string.notif_title))
+                .setContentText(getString(R.string.notif_txt))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        assert notificationManager != null;
+        notificationManager.notify(123, builder.build());
+    }
+
+    private void disableNotifications() {
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        if (notificationManager != null) notificationManager.cancelAll();
     }
 
     private void setUserNameFromToken(String token, TextView textView){
@@ -199,8 +259,9 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     public static boolean deleteDir(File dir) {
         if (dir != null && dir.isDirectory()) {
             String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
+            assert children != null;
+            for (String child : children) {
+                boolean success = deleteDir(new File(dir, child));
                 if (!success) {
                     return false;
                 }
@@ -208,6 +269,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         }
 
         // The directory is now empty so delete it
+        assert dir != null;
         return dir.delete();
     }
 
