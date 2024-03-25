e45a6baa088277f45196d699975c5dfad3abaa3e
Rémi <contact@remilemaire.fr>
2024-03-25 18:29:20 +0100

    eco coins

--- app/src/main/java/iut/dam/gestionresidence/ui/fragments/user_account/UserAccountFragment.java
+++ app/src/main/java/iut/dam/gestionresidence/ui/fragments/user_account/UserAccountFragment.java
@@ -1,14 +1,15 @@
 package iut.dam.gestionresidence.ui.fragments.user_account;
 
-import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,18 +18,22 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import iut.dam.gestionresidence.R;
 import iut.dam.gestionresidence.databinding.FragmentUserAccountBinding;
@@ -36,10 +41,25 @@ import iut.dam.gestionresidence.entities.TokenManager;
 
 public class UserAccountFragment extends Fragment {
     private FragmentUserAccountBinding binding;
+    private ActivityResultLauncher<Intent> galleryLauncher;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentUserAccountBinding.inflate(inflater, container, false);
+
+        ImageView imgUser = binding.imgUserProfile;
+        String urlString = "http://remi-lem.alwaysdata.net/gestionResidence/getProfilePicture.php?token="+TokenManager.getToken();
+        Ion.with(this)
+                .load(urlString)
+                .asBitmap()
+                .setCallback((e, bitmap) -> {
+                    if (e == null && bitmap != null) {
+                        imgUser.setImageBitmap(bitmap);
+                    } else {
+                        imgUser.setImageResource(R.drawable.user_logo);
+                    }
+                });
+
         return binding.getRoot();
     }
 
@@ -70,27 +90,49 @@ public class UserAccountFragment extends Fragment {
 
         ImageView imgUserProfile = binding.imgUserProfile;
 
-        ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+        galleryLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(), result -> {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent data = result.getData();
-                        Uri uri = data.getData();
-                        try {
-                            Bitmap bitmap = MediaStore.Images.Media
-                                    .getBitmap(requireActivity().getContentResolver(), uri);
-                            imgUserProfile.setImageBitmap(bitmap);
-                        } catch (Exception e) {
-                            Toast.makeText(getActivity(), R.string.error_load_img,
-                                    Toast.LENGTH_SHORT).show();
+                        if (data != null) {
+                            Uri selectedImageUri = data.getData();
+                            // Assuming you have a method to convert Uri to byte[]
+                            byte[] imageData = convertUriToByteArray(selectedImageUri);
+                            if (imageData != null) {
+                                Bitmap bitmap = null;
+                                try {
+                                    bitmap = MediaStore.Images.Media
+                                            .getBitmap(requireActivity().getContentResolver(), selectedImageUri);
+                                } catch (IOException e) {
+                                    Log.e(TAG, "Erreur : " + e.getMessage());
+                                }
+                                imgUserProfile.setImageBitmap(bitmap);
+                                sendImageDataToServer(imageData);
+                            }
                         }
                     }
                 });
 
         imgUserProfile.setOnClickListener(v -> openGallery(galleryLauncher));
 
-        TextView textViewEcoCoin = binding.txtEcoCoin;
+        setNbEcoCoins(binding.txtEcoCoin);
+    }
 
-        textViewEcoCoin.setText(getString(R.string.my_eco_coins_text, 5)); //TODO nb eco coins
+    private void setNbEcoCoins(TextView textViewEcoCoin) {
+        String urlString = "http://remi-lem.alwaysdata.net/gestionResidence/getUserName.php?token="
+                + TokenManager.getToken();
+        Ion.with(this).load(urlString).asString().setCallback((e, result) -> {
+            int nb_coins;
+            JSONObject jsonObject;
+            try {
+                jsonObject = new JSONObject(result);
+                nb_coins = Integer.parseInt(jsonObject.getString("nb_coins"));
+            } catch (JSONException ex) {
+                nb_coins = 5;
+            }
+            textViewEcoCoin.setText(getString(R.string.my_eco_coins_text, nb_coins));
+        });
+
     }
 
     private void btnConfirmChangesClicked(EditText editTextFirstName, EditText editTextLastName,
@@ -165,6 +207,53 @@ public class UserAccountFragment extends Fragment {
     private void openGallery(ActivityResultLauncher<Intent> galleryLauncher) {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         galleryLauncher.launch(intent);
+    }
+
+    private void sendImageDataToServer(byte[] imageData) {
+        String url = "http://remi-lem.alwaysdata.net/gestionResidence/updateProfilePicture.php?token="
+        + TokenManager.getToken();
+
+        Ion.with(requireContext())
+                .load("POST", url)
+                .setMultipartParameter("photo", Base64.encodeToString(imageData, Base64.DEFAULT))
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        if (result.equals("OK")) {
+                            new AlertDialog.Builder(getActivity())
+                                    .setTitle(getString(R.string.success_change_user_parameters))
+                                    .setPositiveButton(android.R.string.ok, (dialog, which) ->
+                                            dialog.dismiss())
+                                    .show();
+                        } else {
+                            new AlertDialog.Builder(getActivity())
+                                    .setTitle(getString(R.string.error_change_user_parameters))
+                                    .setPositiveButton(android.R.string.ok, (dialog, which) ->
+                                            dialog.dismiss())
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+
+    // Helper method to convert Uri to byte[]
+    private byte[] convertUriToByteArray(Uri uri) {
+        try {
+            InputStream inputStream = requireActivity().getContentResolver().openInputStream(uri);
+            ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+                byteBuffer.write(buffer, 0, len);
+            }
+            return byteBuffer.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
